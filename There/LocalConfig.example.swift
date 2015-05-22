5fabb8dc401da528c0e255496ffee99501ae7df3
Carsten Witzke <carsten@staticline.de>
2015-05-22 19:46:41 +0200

    added LocalConfig-template for private data

---
+++ There/LocalConfig.example.swift
@@ -0,0 +1,21 @@
+//
+//  LocalConfig.swift
+//  There
+//
+//  Local app configuration and place for all private keys & configurations
+//
+//  Created by Carsten Witzke on 22/05/15.
+//  Copyright (c) 2015 staticline. All rights reserved.
+//
+
+import UIKit
+
+class LocalConfig: NSObject {
+    class func hereAppID() -> String {
+        return "<your ID>"
+    }
+    
+    class func hereAppCode() -> String {
+        return "<your code>"
+    }
+}
