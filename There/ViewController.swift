13077398609e61b78673af209c9fb380cff77110
Carsten Witzke <carsten@staticline.de>
2015-05-22 18:09:39 +0200

    Initial Commit

---
+++ There/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  There
+//
+//  Created by Carsten Witzke on 22/05/15.
+//  Copyright (c) 2015 staticline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
