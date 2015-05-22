3cbb4576738d2d12c9d6df80c75000b72bb74203
Carsten Witzke <carsten@staticline.de>
2015-05-22 18:27:06 +0200

    boilerplate init

---
+++ There/MapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapViewController.swift
+//  There
+//
+//  Created by Carsten Witzke on 22/05/15.
+//  Copyright (c) 2015 staticline. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
