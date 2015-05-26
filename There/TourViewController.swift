1f18231da27096c6c3fdbdf42ee5e818b55c2129
Carsten Witzke <carsten@staticline.de>
2015-05-26 22:02:07 +0200

    tweaked tour overview

--- There/TourViewController.swift
+++ There/TourViewController.swift
@@ -10,12 +10,77 @@ import UIKit
 
 class TourViewController: UITableViewController {
 
+    @IBOutlet weak var tableHeaderLabel: UILabel!
     var model:TourModel!
+    lazy var hintView:UIView = {
+        // very simple tableview background
+        var hintView = UIView(frame: self.tableView.bounds)
+        hintView.autoresizingMask = .FlexibleHeight | .FlexibleWidth
+        
+        let iv = UIImageView(image: UIImage(named: "hint"))
+        iv.frame = hintView.bounds
+        iv.alpha = 0.3
+        iv.contentMode = UIViewContentMode.ScaleAspectFit
+        hintView.addSubview(iv)
+        
+        let label = UILabel(frame: iv.frame)
+        label.font = UIFont.boldSystemFontOfSize(32)
+        label.text = NSLocalizedString("label.hint.nowps", comment: "a hint how to add new waypoints")
+        label.textColor = UIColor.blackColor()
+        label.textAlignment = NSTextAlignment.Center
+        label.numberOfLines = 0
+        hintView.addSubview(label)
+        
+        return hintView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        updateTourFacts()
+    }
+    
+    private func updateTourFacts() {
+        if count(model.tourItems) > 0 {
+            let nf = NSNumberFormatter()
+            nf.numberStyle = NSNumberFormatterStyle.DecimalStyle
+            
+            let items = count(model.tourItems)
+            let numPoints = nf.stringFromNumber(items)!
+            
+            // .stringsdict is better than this!
+            // http://cldr.unicode.org/index/cldr-spec/plural-rules
+            var format = ""
+            if items == 0 {
+                format = NSLocalizedString("label.tour.legs.zero", comment: "number of waypoints in the tour")
+            } else if items == 1 {
+                format = NSLocalizedString("label.tour.legs.one", comment: "number of waypoints in the tour")
+            } else {
+                format = NSLocalizedString("label.tour.legs.other", comment: "number of waypoints in the tour")
+            }
+            
+            var string = String(format: format, numPoints)
+            
+            tableHeaderLabel.text = string
+            
+            tableView.tableHeaderView?.hidden = false
+            self.editButtonItem().enabled = true
+        } else {
+            tableView.tableHeaderView?.hidden = true
+            self.editButtonItem().enabled = false
+        }
+    }
+    
+    private func setHint(visible showView:Bool, animated animate:Bool) {
+        let duration = animate ? 0 : 0.2
+        UIView.animateWithDuration(duration, animations: { () -> Void in
+            if showView {
+                self.tableView.backgroundView = self.hintView
+            } else {
+                self.hintView.removeFromSuperview()
+            }
+        })
     }
 
     // MARK: - Table view data source
@@ -25,7 +90,9 @@ class TourViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return count(model.tourItems)
+        let rows = count(model.tourItems)
+        setHint(visible: (rows == 0), animated: true)
+        return rows
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -54,6 +121,7 @@ class TourViewController: UITableViewController {
         if editingStyle == .Delete {
             model.removeItem(at: indexPath)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            updateTourFacts()
         }
     }
 }
